c3a873b11cf75f1e9052e4b14d6645cea2f82aff
iain <iainfraser188@msn.com>
2022-01-26 15:09:28 +0000

    database added

--- src/main/java/com/codeClan/example/Poker/game/models/Player.java
+++ src/main/java/com/codeClan/example/Poker/game/models/Player.java
@@ -1,18 +1,43 @@
 package com.codeClan.example.Poker.game.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
+
+@Entity
+@Table(name="players")
 public class Player {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
     private String name;
+
+    @Column(name="stack")
     private double stack;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Transient
     private List<Card> hand;
 
+    public Player() {
+    }
 
-    public Player(String name, double stack, List<Card> hand) {
+    public Player(String name, double stack, String username, String password) {
         this.name = name;
         this.stack = stack;
-        this.hand = hand;
+        this.username = username;
+        this.password = password;
+        this.hand = new ArrayList<Card>();
+
     }
 
     public String getName() {
@@ -37,6 +62,30 @@ public class Player {
 
     public void setHand(List<Card> hand) {
         this.hand = hand;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public double addToStack(double amount){
