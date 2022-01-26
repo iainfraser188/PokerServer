ba13ec150890a6a1e6d6b40995248c80b463f3d9
StrtMilne <oober@hotmail.co.uk>
2022-01-26 11:05:45 +0000

    refactored handcalculator to work with exsiting classes

--- src/main/java/com/codeClan/example/Poker/game/models/HandCalculator.java
+++ src/main/java/com/codeClan/example/Poker/game/models/HandCalculator.java
@@ -1,25 +1,26 @@
 package com.codeClan.example.Poker.game.models;
 
-import javax.smartcardio.Card;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Comparator;
+import java.util.List;
 
 public class HandCalculator {
 
     // CALCULATES SETS
 
     public int isFourOfAKind(PlayerHand playerHand) {
-        Collections.sort(playerHand);
+        List<Card> cards = playerHand.getPlayerCards();
+        Collections.sort(cards, Comparator.comparingInt(card -> card.getCardValue().getValue()));
         int isFour = 0;
-        for (int i = 0; i < playerHand.size() - 3; i++ ) {
+        for (int i = 0; i < cards.size() - 3; i++ ) {
 
             // Requires getIntValue() after initial get() function.
 
-            if(playerHand.get(i) == playerHand.get(i+1)
-                    && playerHand.get(i+1) == playerHand.get(i+2)
-                    && playerHand.get(i+2) == playerHand.get(i+3)) {
-                isFour = playerHand.get(i).getIntValue();
+            if(cards.get(i).getCardValue().getValue() == cards.get(i+1).getCardValue().getValue()
+                    && cards.get(i+1).getCardValue().getValue()  == cards.get(i+2).getCardValue().getValue()
+                    && cards.get(i+2).getCardValue().getValue()  == cards.get(i+3).getCardValue().getValue() ) {
+                isFour = cards.get(i).getCardValue().getValue() ;
             }
         }
         if(isFour == 1){
@@ -29,15 +30,16 @@ public class HandCalculator {
     }
 
     public int isThreeOfAKind(PlayerHand playerHand) {
-        Collections.sort(playerHand);
+        List<Card> cards = playerHand.getPlayerCards();
+        Collections.sort(cards, Comparator.comparingInt(card -> card.getCardValue().getValue()));
         int value = 0;
         boolean aces = false;
 
         // Requires getIntValue() after initial get() function.
-        for (int i = 0; i < playerHand.size() - 2; i++ ) {
-            if( playerHand.get(i) == playerHand.get(i+1)
-                    && playerHand.get(i+1) == playerHand.get(i+2)) {
-                value = playerHand.get(i);
+        for (int i = 0; i < cards.size() - 2; i++ ) {
+            if( cards.get(i).getCardValue().getValue() == cards.get(i+1).getCardValue().getValue()
+                    && cards.get(i+1).getCardValue().getValue() == cards.get(i+2).getCardValue().getValue()) {
+                value = cards.get(i).getCardValue().getValue();
                 if (value == 1){
                     aces = true;
                 }
@@ -50,14 +52,15 @@ public class HandCalculator {
     }
 
     public int isPair(PlayerHand playerHand) {
-        Collections.sort(playerHand);
+        List<Card> cards = playerHand.getPlayerCards();
+        Collections.sort(cards, Comparator.comparingInt(card -> card.getCardValue().getValue()));
         int value = 0;
         boolean aces = false;
 
         // Requires getIntValue() after initial get() function.
-        for (int i = 0; i < playerHand.size() - 1; i++ ) {
-            if(playerHand.get(i) == playerHand.get(i+1)) {
-                value = playerHand.get(i).getIntValue();
+        for (int i = 0; i < cards.size() - 1; i++ ) {
+            if(cards.get(i).getCardValue().getValue() == cards.get(i+1).getCardValue().getValue()) {
+                value = cards.get(i).getCardValue().getValue();
                 if (value == 1){
                     aces = true;
                 }
@@ -73,6 +76,7 @@ public class HandCalculator {
     // CALCULATES FLUSH
 
     public int isFlush(PlayerHand playerHand) {
+        List<Card> cards = playerHand.getPlayerCards();
         int value = 0;
         boolean ace = false;
         ArrayList<Card> hearts = new ArrayList<>();
@@ -80,52 +84,50 @@ public class HandCalculator {
         ArrayList<Card> clubs = new ArrayList<>();
         ArrayList<Card> spades = new ArrayList<>();
 
-        for (int i = 0; i < playerHand.size(); i++) {
+        for (int i = 0; i < cards.size(); i++) {
 
-        // Needs extra getter before equality
-
-            if (playerHand.get(i) == hearts){
-                hearts.add(playerHand.get(i));
-            } else if (playerHand.get(i) == diamonds){
-                diamonds.add(playerHand.get(i));
-            } else if (playerHand.get(i) == clubs){
-                clubs.add(playerHand.get(i));
-            } else if (playerHand.get(i) == spades) {
-                spades.add(playerHand.get(i));
+            if (cards.get(i).getCardSuitType() == CardSuitType.HEARTS){
+                hearts.add(cards.get(i));
+            } else if (cards.get(i).getCardSuitType() == CardSuitType.DIAMONDS){
+                diamonds.add(cards.get(i));
+            } else if (cards.get(i).getCardSuitType() == CardSuitType.CLUBS){
+                clubs.add(cards.get(i));
+            } else if (cards.get(i).getCardSuitType() == CardSuitType.SPADES) {
+                spades.add(cards.get(i));
             }
         }
 
         // Needs getIntValue added properly and sort function set up
         if (hearts.size() >= 4){
-            hearts.sort(hearts.get(i).getIntValue()...)
+            Collections.sort(hearts, Comparator.comparingInt(card -> card.getCardValue().getValue()));
             int size = hearts.size();
-            int highest = hearts.get(size-1).getIntValue()...;
+            int highest = hearts.get(size-1).getCardValue().getValue();
             value = highest;
-            if(hearts.get(0).getIntValue() == 1){
+            if(hearts.get(0).getCardValue().getValue() == 1){
                 value = 14;
             }
         } else if (diamonds.size() >= 4){
-            diamonds.sort(diamonds.get(i).getIntValue()...)
+            Collections.sort(diamonds, Comparator.comparingInt(card -> card.getCardValue().getValue()));
             int size = diamonds.size();
-            int highest = diamonds.get(size-1).getIntValue()...;
+            int highest = diamonds.get(size-1).getCardValue().getValue();
             value = highest;
-            if(diamonds.get(0).getIntValue() == 1){
+            if(diamonds.get(0).getCardValue().getValue() == 1){
                 value = 14;
             }
         } else if (clubs.size() >= 4){
-            clubs.sort(clubs.get(i).getIntValue()...)
+            Collections.sort(clubs, Comparator.comparingInt(card -> card.getCardValue().getValue()));
             int size = clubs.size();
-            int highest = clubs.get(size-1).getIntValue()...;
+            int highest = clubs.get(size-1).getCardValue().getValue();
             value = highest;
-            if(clubs.get(0).getIntValue() == 1){
+            if(clubs.get(0).getCardValue().getValue() == 1){
                 value = 14;
             }
         } else if (spades.size() >= 4) {
-            spades.sort(spades.get(i).getIntValue()...)
+            Collections.sort(spades, Comparator.comparingInt(card -> card.getCardValue().getValue()));
             int size = spades.size();
-            int highest = spades.get(size-1).getIntValue()...;
+            int highest = spades.get(size-1).getCardValue().getValue();
             value = highest;
-            if(spades.get(0).getIntValue() == 1){
+            if(spades.get(0).getCardValue().getValue() == 1){
                 value = 14;
             }
         }
@@ -134,23 +136,24 @@ public class HandCalculator {
     }
 
     public int isStraight(PlayerHand playerHand) {
-        Collections.sort(playerHand);
+        List<Card> cards = playerHand.getPlayerCards();
+        Collections.sort(cards, Comparator.comparingInt(card -> card.getCardValue().getValue()));
         int consecutive = 1;
         int straight = 0;
-        for (int i = 0; i < playerHand.size() - 4; i++) {
-            if(playerHand.get(i).getIntValue() + 1 == playerHand.get(i+1).getIntValue) {
+        for (int i = 0; i < cards.size() - 4; i++) {
+            if(cards.get(i).getCardValue().getValue() + 1 == cards.get(i+1).getCardValue().getValue()) {
                 consecutive++;
                 if (consecutive > 4) {
-                    straight = playerHand.get(i+1);
+                    straight = cards.get(i+1).getCardValue().getValue();
                 }
-            } else if (playerHand.get(i).getIntValue() == playerHand.get(i+1).getIntValue) {
+            } else if (cards.get(i).getCardValue().getValue() == cards.get(i+1).getCardValue().getValue()) {
                 //nothing required
             } else {
                 consecutive = 1;
             }
         }
 
-        if(consecutive >= 4 && playerHand.get(0).getIntValue() == 1) {
+        if(consecutive >= 4 && cards.get(0).getCardValue().getValue() == 1) {
             straight = 14;
         }
 
@@ -158,7 +161,9 @@ public class HandCalculator {
     }
 
     public int isStraightFlush(PlayerHand playerHand) {
+        int value = 0;
         // Use flush calculator and then straight calculator;
+        return value;
     }
 
 //    DOESNT WORK AS PAIR ALWAYS RETURNS HIGHEST PAIR, MAY CLASH WITH TRIPS
